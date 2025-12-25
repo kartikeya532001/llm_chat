6011c1b2ccb1b5e60ba4127dd98026143c63953c
kartikeya532001 <kartikeya532001@gmail.com>
2025-12-25 09:57:14 +0000

    Update nodegroup config

--- infra/terraform/nodegroups.tf
+++ infra/terraform/nodegroups.tf
@@ -1,11 +1,11 @@
 # ======================================
 # EKS Cluster
 # ======================================
-module "eksnode" {
+module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-  cluster_name    = "var.cluster_name"
+  cluster_name    = var.cluster_name
   cluster_version = "1.30"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
