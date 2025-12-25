6206eb53a3ce3aa9283cf3d4d108777ed019e8b5
kartikeya532001 <kartikeya532001@gmail.com>
2025-12-25 11:24:53 +0000

    updated nodegroup

---
+++ infra/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.30.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.25.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.0"
+    }
+  }
+}
