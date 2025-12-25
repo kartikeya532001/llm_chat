c60d72dbbaa566e7f0b85ff1c15c6c558bb8574d
kartikeya532001 <kartikeya532001@gmail.com>
2025-12-25 12:11:00 +0000

    updated nodegroup

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -2,15 +2,16 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.0.2"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  name    = var.cluster_name
+  version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   enable_irsa = true
 
-  cluster_endpoint_public_access = true
+  # cluster endpoint access moved under 'cluster_endpoint_public_access' block
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
   tags = {
     Environment = "prod"
