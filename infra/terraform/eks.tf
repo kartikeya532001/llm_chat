fca6c2d643375ce81bbab33708ccb76d315792a5
kartikeya532001 <kartikeya532001@gmail.com>
2025-12-25 12:27:16 +0000

    Version Issues

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.0.2"
 
-  cluster_name    = var.cluster_name
+  name    = var.cluster_name
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
