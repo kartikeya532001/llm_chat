69b13053ca71f709b279e7363083b2a1f4ef6f10
kartikeya532001 <kartikeya532001@gmail.com>
2025-12-25 11:45:54 +0000

    updated nodegroup

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.5.0, < 1.7.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.30.0"
+      version = "~> 4.70"
     }
 
     kubernetes = {
