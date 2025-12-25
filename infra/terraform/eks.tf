a42079a38f40b6e5a4291699ccee39663d430d00
kartikeya532001 <kartikeya532001@gmail.com>
2025-12-25 12:32:00 +0000

    Version Issues

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -3,14 +3,14 @@ module "eks" {
   version = "21.0.2"
 
   name    = var.cluster_name
-  cluster_version = "1.29"
+  kubernetes_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   enable_irsa = true
 
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  cluster_endpoint_public_access = ["0.0.0.0/0"]
 
   tags = {
     Environment = "prod"
