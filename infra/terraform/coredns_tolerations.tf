61db72dcc46b485ac57a0499eed9deb5f66d2d23
kartikeya532001 <kartikeya532001@gmail.com>
2025-12-25 13:55:09 +0000

    Updated coredns file

--- infra/terraform/coredns_tolerations.tf
+++ infra/terraform/coredns_tolerations.tf
@@ -1,30 +1,30 @@
-resource "kubernetes_manifest" "coredns_tolerations" {
-  manifest = {
-    apiVersion = "apps/v1"
-    kind       = "Deployment"
-    metadata = {
-      name      = "coredns"
-      namespace = "kube-system"
-    }
-    spec = {
-      template = {
-        spec = {
-          tolerations = [
-            {
-              key      = "warm-pool"
-              operator = "Equal"
-              value    = "true"
-              effect   = "NoSchedule"
-            },
-            {
-              key      = "spot"
-              operator = "Equal"
-              value    = "true"
-              effect   = "PreferNoSchedule"
-            }
-          ]
-        }
-      }
+resource "null_resource" "coredns_tolerations" {
+  depends_on = [module.eks]
+
+  provisioner "local-exec" {
+    command = <<EOT
+kubectl apply -f - <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: coredns
+  namespace: kube-system
+spec:
+  template:
+    spec:
+      tolerations:
+      - key: warm-pool
+        operator: Equal
+        value: "true"
+        effect: NoSchedule
+      - key: spot
+        operator: Equal
+        value: "true"
+        effect: PreferNoSchedule
+EOF
+EOT
+    environment = {
+      KUBECONFIG = module.eks.kubeconfig_path
     }
   }
 }
